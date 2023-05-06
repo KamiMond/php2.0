1b4119c3dec11d4f231290b0027f8cfad9eb6945
KamiMond <95546039+KamiMond@users.noreply.github.com>
2023-05-06 19:19:31 +0300

    Add files via upload

---
+++ cli.php
@@ -0,0 +1,20 @@
+<?php
+
+use GeekBrains\LevelTwo\Blog\Command\Arguments;
+use GeekBrains\LevelTwo\Blog\Command\CreateUserCommand;
+use GeekBrains\LevelTwo\Blog\Repositories\UsersRepository\SqliteUsersRepository;
+
+
+include __DIR__ . "/vendor/autoload.php";
+
+$connection = new PDO('sqlite:' . __DIR__ . '/blog.sqlite');
+
+$usersRepository = new SqliteUsersRepository($connection);
+
+$command = new CreateUserCommand($usersRepository);
+
+try {
+    $command->handle(Arguments::fromArgv($argv));
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
